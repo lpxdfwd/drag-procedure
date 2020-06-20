5b489dc313884d77877fa7564a85eec08ce999ad
lpxdfwdjm <920331287@qq.com>
2020-06-20 22:24:46 +0800

    commit

--- environment/development.env.js
+++ environment/development.env.js
@@ -1,3 +1,6 @@
 module.exports = {
+  publicPath: '/',
   production: false,
+  servicePath: '',
+  basePath: '/',
 };
--- environment/production.env.js
+++ environment/production.env.js
@@ -1,3 +1,6 @@
 module.exports = {
+  publicPath: '/management/',
   production: true,
+  servicePath: 'http://112.124.202.207:8080',
+  basePath: '/',
 };
